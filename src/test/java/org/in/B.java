be2cbe9dfdff99f32d60f6b0a79bfdf4bd7faa1b
swadaksi <sswathi1917@gmail.com>
2024-09-12 18:57:34 +0530

    message

---
+++ src/test/java/org/in/B.java
@@ -0,0 +1,16 @@
+package org.in;
+
+public class B {
+private void swa() {
+	// TODO Auto-generated method stub
+
+}
+private void priya() {
+	// TODO Auto-generated method stub
+
+}
+private void velan() {
+	// TODO Auto-generated method stub
+
+}
+}
