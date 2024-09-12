0177cd9facdbf5f64c0ba4865837ed8307bca3a7
swadaksi <sswathi1917@gmail.com>
2024-09-12 18:39:52 +0530

    message

---
+++ src/test/java/org/in/Simplegit.java
@@ -0,0 +1,16 @@
+package org.in;
+
+public class Simplegit {
+private void m1() {
+	// TODO Auto-generated method stub
+
+}
+private void m2() {
+	// TODO Auto-generated method stub
+
+}
+private void m3() {
+	// TODO Auto-generated method stub
+
+}
+}
